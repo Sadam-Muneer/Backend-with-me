07b6d67b4b8afd7a0d76d5e6c9380a2981e4e87f
Muhammad Sadam Muneer <sadammuneer39@gmail.com>
2024-09-03 15:09:44 +0500

    modules, import&export and lodash

---
+++ backend/notes.js
@@ -0,0 +1,6 @@
+console.log("Notes file is loading");
+var age = 2;
+const addnumber = (a, b) => {
+  return a + b;
+};
+module.exports = { age, addnumber };
---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const os = require("os");
+const _ = require("lodash");
+const Notes = require("./notes");
+const user = os.userInfo();
+console.log(user.username);
+fs.appendFile("sadam.txt", "hy+ \n", () => console.log("File is created"));
+var age = Notes.age;
+const result = Notes.addnumber(age + 18, 2);
+console.log(result);
+//-------------
+const data = ["person", "person", 1, 2, 1, 3, 4, 5, 2, 3, 4, 5, 2, "Hello"];
+const filter = _.uniq(data);
+console.log(filter);
+console.log(_.isString(3));
